d2f85be54959aa0f7a815acef3a5a3dc7e2553c0
Marzena Laszuk <52258431+laszukem@users.noreply.github.com>
2019-11-08 20:08:42 +0100

    Update my.js

--- calc/my.js
+++ calc/my.js
@@ -2,7 +2,7 @@ const calculator = {
   displayValue: '0'
 };
 
-function updateDisplay() {
+function updateDisplay(displayValue) {
   const display = document.querySelector('.calculator-screen');
   display.value = calculator.displayValue;
 }
